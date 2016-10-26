118f32081927f7f2c75516c9dc5723249c13eb3c
jes4u <jes4u@uw.edu>
2016-10-25 19:09:29 -0700

    Lab

--- exercise-4/exercise.r
+++ exercise-4/exercise.r
@@ -1,30 +1,43 @@
 # Install and load the jsonlite package
-
+setwd("C:/Users/Jesse/Desktop/AUT16/INFO 201/Modules/m10-apis/exercise-4")
+#install.packages("jsonlite")
+library(jsonlite)
 # Make a variable base.url that has the same base url from the omdb documentation.
 # (Hint: visit https://www.omdbapi.com/ to find the base url)
+base.url <- 'http://www.omdbapi.com/?'
 
 # Make a variable called movie that has the names of your favorite movie
+movie <- "Frozen"
 
 # Make a variable called move.no.spaces that holds movie where all the spaces
 # are replaced with the '+' character
+move.no.spaces <- gsub(" ", "+", movie)
 
 # Make a variable called "parameters" that holds a string with the parameters
 # to pass to the API. View the OMDb documentation to see which parameters
 # are available. Remember to separate parameters with the '&' character.
+parameters <- paste0('t=', move.no.spaces, '&year=&plot=short&r=json')
 
 # Make a variable called request that is a string of a request URL made up of the base URL
 # and the parameters string
+request <- paste0(base.url, parameters)
 
 # Use fromJSON to retrieve JSON data from the omdb api using your request URL.
 # Store the result in a variable called movie.data
+movie.data <- fromJSON(request)
 
 # Make movie_data into a data frame using as.data.frame
+movie_data <- as.data.frame(movie.data)
 
 # Write a function called Director that accepts a data frame of movie info and returns
 # A vector of strings that states a movie and the director of said movie.
+Director <- function(movie_data_frame){
+  result <- paste("Title:",movie_data_frame$Title, "Directors: ", movie_data_frame$Director)
+  return(result)
+}
 
 # Call Director with your favorite movie, and assign it to the variable movie.director
-
+answer <- Director(movie_data)
 # Bonus 
 
 # Knowing the director of on movie is okay, but it'd be great to know the directors of different
