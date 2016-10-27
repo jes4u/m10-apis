4224fe7a58d44057c1061591f55528c5bb57fe85
jes4u <jes4u@uw.edu>
2016-10-27 14:35:44 -0700

    ldkf

--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -3,7 +3,7 @@ library(jsonlite)
 library(dplyr)
 
 # Write a function that allows you to specify a movie, then does the following:
-  
+
   # Replace all of the spaces in your movie title with plus signs (+)
   
   
@@ -25,6 +25,22 @@ library(dplyr)
   
   # Return an list of the three pieces of information from above
   
-
+myMovie <- function(movie_name){
+  movie_noSpace <- gsub(" ", "+", movie_name)
+  base_URL <- "https://api.nytimes.com/svc/movies/v2/reviews/search.json?"
+  api_key <- "4334cc67b3d449bb9a1955e3d93cf537"
+  request <- paste0(base_URL, 'query=', movie_noSpace, api_key)
+  results <- fromJSON(request)
+  flattened <- flatten(results$results)
+  
+  first.review <- flattened[1,]
+  headline <- first.review$headline
+  summary <- first.review$summary_short
+  link <- first.review$link.url
+  
+  info <- list(headline=headline, summary=summary, link=link)
+  return(info)
+}
+  
 # Test that your function works with a movie of your choice
-
+myMovie('Tarzan')
